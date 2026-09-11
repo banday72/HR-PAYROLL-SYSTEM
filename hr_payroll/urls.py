8a114b10e5014382e9b26b521ede1dcfafc93d67
banday72 <ahmedalibanday76@gmnail.com>
2026-09-11 15:03:01 +0500

    Fix logout error - custom logout view + fix session cookie for HTTP

--- hr_payroll/urls.py
+++ hr_payroll/urls.py
@@ -3,11 +3,19 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+
+def custom_logout(request):
+    logout(request)
+    return redirect('login')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', custom_logout, name='logout'),
     path('', include('employees.urls')),
     path('attendance/', include('attendance.urls')),
     path('leaves/', include('leaves.urls')),
